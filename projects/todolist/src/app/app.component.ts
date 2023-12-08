f6125774312221583867968c377a53f289758093
MartinPAVY <113358964+MartinPAVY@users.noreply.github.com>
2023-12-08 16:09:31 +0100

    First Commit

---
+++ projects/todolist/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+  <h1>Coucou bisous mon pc est mort</h1>
+  `,
+  styles: []
+})
+export class AppComponent {}
